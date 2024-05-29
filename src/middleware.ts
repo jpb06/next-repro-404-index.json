21b76ef86518be939708de231fdfbcf6a3470165
jpb06 <jpb.06@outlook.com>
2024-05-29 16:50:12 +0200

    next?

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   console.info('middleware', request.url, request.nextUrl);
+
+  return NextResponse.next();
 }
