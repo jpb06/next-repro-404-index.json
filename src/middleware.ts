d13fdf5547bb407474ada72543752ec9dd6df2d4
jpb06 <jpb.06@outlook.com>
2024-05-29 17:07:11 +0200

    back to using basepath

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,4 @@ import { NextResponse, type NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   console.info('middleware', request.url, request.nextUrl);
-
-  // return NextResponse.next();
 }
